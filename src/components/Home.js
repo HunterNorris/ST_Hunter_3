a49546441898df9bd746e3579b179cc1a650d982
Garrett Truong <aznkungfubunnee@gmail.com>
2022-07-11 14:05:12 -0500

    gtedit711-pmendofclass

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,17 +5,8 @@ import PostPage from  "./PostPage"
 
 const Home = ({allPosts}) => {
   return (
-    <div className="posts-main-countainer">
+    <div>
       <h1>Posts</h1>
-      {allPosts.length
-        ? allPosts.map((post) => {
-            return post.active ? (
-              <Link to={`/posts/${post._id}`} key={post._id}>
-                <PostPage post={post} />
-              </Link>
-            ) : null;
-          })
-        : null}
     </div>
   );
 };
